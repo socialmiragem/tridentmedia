2bc8ebb3a24aff9a707930a771d84570e2628931
unknown <social.miragem@gmail.com>
2026-06-16 23:01:59 +0500

    home page service section redirection added

--- src/app/frontend/components/ServiceHoverChange.js
+++ src/app/frontend/components/ServiceHoverChange.js
@@ -4,7 +4,18 @@ import React, { useState } from "react";
 
 const ServiceHoverChange = ({ services }) => {
   const [activeCard, setActiveCard] = useState(0);
-
+  const redirection = [
+    "graphic-design",
+    "exhibit-design",
+    "3d-modeling",
+    "photography",
+    "illustration",
+    "woodcraft",
+  ];
+  const getRedirectUrl = (title) => {
+    const slug = title.toLowerCase().replace(/\s+/g, "-");
+    return redirection.includes(slug) ? `/${slug}` : "#";
+  };
   const getStyle = (index) => ({
     opacity: activeCard === index ? 1 : 0,
     visibility: activeCard === index ? "visible" : "hidden",
@@ -36,12 +47,24 @@ const ServiceHoverChange = ({ services }) => {
         <div className="col-md-5" style={{ position: "relative" }}>
           {services.map((item, index) => (
             <div key={index} style={getStyle(index)} className="px-md-0 px-2">
-              <img src={item.image} alt="" className="img-fluid"/>
+              <div class="wrapper position-relative">
+                <img src={item.image} alt="" className="img-fluid w-100" />
+                <div className="position-absolute start-50 top-50 translate-middle">
+                  <a
+                    href={getRedirectUrl(item.title)}
+                    className="bg-accent btn text-uppercase fs-4 fw-semibold"
+                  >
+                    Learn More
+                  </a>
+                </div>
+              </div>
               <div className="cardcontent text-md-start text-center">
                 <p>what’s included</p>
                 <ul className="text-md-start text-center">
                   {item.items.map((text, i) => (
-                    <li className="text-md-start text-center" key={i}>{text}</li>
+                    <li className="text-md-start text-center" key={i}>
+                      {text}
+                    </li>
                   ))}
                 </ul>
               </div>
